7091821c05567f7ccbcf0c7b9fae9e2b0a904d2f
Javier Costa <jacosros@gmail.com>
2018-12-14 17:20:59 +0100

    Fixed baseService get and post

--- frontend/src/app/services/baseService.ts
+++ frontend/src/app/services/baseService.ts
@@ -1,7 +1,7 @@
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {Observable, ObservableInput, of, throwError} from 'rxjs';
 import {Injectable} from '@angular/core';
-import {map} from 'rxjs/operators';
+import {catchError, map} from 'rxjs/operators';
 
 @Injectable()
 export class BaseService {
@@ -23,27 +23,28 @@ export class BaseService {
   };
 
 
+  protected handleError<T>(httpErrorResponse: any, caught: Observable<T>): ObservableInput<T> {
+    if (httpErrorResponse.error.error) {
+      return throwError(httpErrorResponse.error.error);
+    }
+
+    return throwError(httpErrorResponse.error);
+  }
 
   protected serverGet<T>(endpoint: string): Observable<T> {
     return this.http.get<ServerResponse<T>>(endpoint, this.httpOptions)
       .pipe(
-        map(serverRes => {
-          return serverRes.result;
-        })
+        map(serverRes => serverRes.result as T),
+        catchError(this.handleError)
       );
   }
 
   protected serverPost<T>(endpoint: string, data: any): Observable<T> {
-    return new Observable<T>(observer => {
-      this.http.post<ServerResponse<T>>(endpoint, data, this.httpOptions)
-        .subscribe(serverRes => {
-          if (serverRes.ok) {
-            return observer.next(serverRes.result);
-          }
-
-          return observer.error(serverRes.error);
-        }, observer.error);
-    });
+    return this.http.post<ServerResponse<T>>(endpoint, data, this.httpOptions)
+      .pipe(
+        map(serverRes => serverRes.result as T),
+        catchError(this.handleError)
+      );
   }
 }
 
@@ -52,4 +53,3 @@ interface ServerResponse<T> {
   result: T;
   error: string;
 }
-
